5c8f86d5fc575dcf14e7428f0f87854179d0a2a9
Raymond Fang <rayymondf29@gmail.com>
2026-05-09 03:10:28 -0400

    mades changes to help sync with Paas

--- server.js
+++ server.js
@@ -601,7 +601,7 @@ async function start() {
   await initializeDatabase();
   await ensureGuestUser();
   
-  app.listen(PORT, () => {
+  app.listen(PORT,"0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 }
